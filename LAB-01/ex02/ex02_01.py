3629cb89db883d31c549ec06b070fb921a5c1dc3
Nothingtoread <nhon4560@gmail.com>
2025-05-12 10:43:02 +0700

    [add] LAB-01 ex01 ex02

---
+++ LAB-01/ex02/ex02_01.py
@@ -0,0 +1,3 @@
+ten=input("Hãy nhập tên")
+tuoi=input("Hãy nhập tuổi")
+print("Chào",ten,"Bạn",tuoi,"tuổi")
---
+++ LAB-01/ex02/ex02_03.py
@@ -0,0 +1,5 @@
+so =int(input("Nhập số nguyên: "))
+if so%2 ==0:
+    print(so,"Chẵn")
+else:
+    print(so,"Lẻ")
