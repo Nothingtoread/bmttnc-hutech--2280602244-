c2889355ad75bb1d31fc6f6672bdab11bee1a5d7
Nothingtoread <nhon4560@gmail.com>
2025-06-09 10:15:33 +0700

    [add] lab-05

---
+++ LAB-05/img-hidden/decrypt.py
@@ -0,0 +1,43 @@
+import sys
+from PIL import Image
+
+def decode_image(encoded_image_path):
+    img = Image.open(encoded_image_path)
+    width, height = img.size
+    binary_message = ""
+
+    for row in range(height):
+        for col in range(width):
+            pixel = img.getpixel(((col, row)))
+
+            for color_channel in range(3):
+                binary_message += format(pixel[color_channel], '08b')[-1]
+
+    # Tìm dấu kết thúc thông điệp
+    message_end_indicator = '1111111111111110'
+    try:
+        end_index = binary_message.index(message_end_indicator)
+    except ValueError:
+        print("No hidden message found or message corrupted.")
+        return ""
+
+    binary_message = binary_message[:end_index]
+
+    decoded_message = ""
+    for i in range(0, len(binary_message), 8):
+        byte = binary_message[i:i+8]
+        decoded_message += chr(int(byte, 2))
+
+    return decoded_message
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python decrypt.py <encoded_image_path>")
+        return
+
+    encoded_image_path = sys.argv[1]
+    decoded_message = decode_image(encoded_image_path)
+    print("Decoded message:", decoded_message)
+
+if __name__ == "__main__":
+    main() 
